dd4082a70eaeffddfed119e67de6d7151dc03139
ankit9119190756 <47775192+ankit9119190756@users.noreply.github.com>
2023-04-16 14:19:44 +0530

    Add files via upload

---
+++ BmsIndia/ProductImport/registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Copyright © BmsIndia @Copyright All rights reserved.
+ * See COPYING.txt for license details.
+ */
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'BmsIndia_ProductImport', __DIR__);
+
